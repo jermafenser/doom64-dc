197c7846aad838e584e96694ad81662e1d5d17e9
jnmartin84 <jnmartin84@gmail.com>
2025-01-12 09:07:12 -0500

    make clean should now remove EXE file if one exists, fixes #50

--- wadtool/clean.sh
+++ wadtool/clean.sh
@@ -6,6 +6,8 @@ rm -f "$SCRIPT_DIR/imgproc.o"
 rm -f "$SCRIPT_DIR/wadtool.o"
 rm -f "$SCRIPT_DIR/mapconv.o"
 rm -f "$SCRIPT_DIR/wadtool"
+# sorry Windows users, I made your life harder by overlooking this
+rm -f "$SCRIPT_DIR/wadtool.exe"
 rm -f "$SCRIPT_DIR/../selfboot/alt.wad"
 rm -f "$SCRIPT_DIR/../selfboot/pow2.wad"
 rm -f "$SCRIPT_DIR/../selfboot/tex/non_enemy.tex"
